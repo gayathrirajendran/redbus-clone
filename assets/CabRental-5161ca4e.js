d22b763d807fb7bc6b9fa2644220a238ac7aa711
gayathri <gayathriraj.vlr@gmail.com>
2023-07-27 00:38:01 +0530

    Updates

---
+++ assets/CabRental-5161ca4e.js
@@ -0,0 +1,1 @@
+import{H as t}from"./index-52370203.js";const e=()=>t.jsx("div",{children:"CabRental"});export{e as default};
---
+++ assets/TrainTickets-80644c20.js
@@ -0,0 +1,1 @@
+import{H as r}from"./index-52370203.js";const i=()=>r.jsx("div",{children:"TrainTickets"});export{i as default};
